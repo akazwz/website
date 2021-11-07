2d001c2ff6066cb6c30788eb954d43e107b0618f
WenZhuo Zhao <akazwz@pm.me>
2021-11-07 16:51:30 +0800

    update projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,73 +1,67 @@
 import Layout from '../src/components/layout'
-import { Box, Image, Flex, Heading, SimpleGrid, Spacer, Center, Button, useBoolean, Link } from '@chakra-ui/react'
+import Image from 'next/image'
+import {Heading, SimpleGrid, Center, Link} from '@chakra-ui/react'
 import styles from '../styles/Projects.module.css'
 import React from 'react'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 
 const Projects = () => {
-  const router = useRouter()
-  return (
-    <>
-      <Layout>
-        <Heading size="xl" textAlign="center" mb="1rem">
-          Created Things
-        </Heading>
-        <SimpleGrid columns={[1, null, 2]}>
-          <Flex
-            height="70vh"
-            flexDirection="column"
-            padding="2rem"
-            marginTop={3}
-            className={styles.card}
-          >
-            <Box
-              width="100%"
-              height="65vh"
-              backgroundImage="https://bit.ly/2Z4KKcF"
-              borderRadius="lg"
-              overflow="hidden"
-            >
-            </Box>
-            <Link
-              mr="1.5rem"
-              onClick={() => router.push('/projects')}
-              style={{
-                fontSize: '1.5rem',
-                fontWeight: 'bolder',
-              }}
-            >
-              Project One
-            </Link>
-          </Flex>
-          <Flex
-            height="70vh"
-            flexDirection="column"
-            padding="2rem"
-            marginTop={3}
-            className={styles.card}
-          >
-            <Box
-              width="100%"
-              height="65vh"
-              backgroundImage="https://bit.ly/2Z4KKcF"
-              borderRadius="lg"
-              overflow="hidden"
-            >
-            </Box>
-            <Link
-              onClick={() => router.push('/projects')}
-              style={{
-                fontSize: '1.5rem',
-                fontWeight: 'bolder',
-              }}
-            >
-              Project Two
-            </Link>
-          </Flex>
-        </SimpleGrid>
-      </Layout>
-    </>
-  )
+    const router = useRouter()
+    const projects = [
+        {
+            imageSrc: '/hot.png',
+            projectName: "Q HotSearch Web"
+        },
+        {
+            imageSrc: '/mini.jpg',
+            projectName: 'Z HotSearch MiniProgram'
+        },
+    ]
+
+    const ProjectCards = () => {
+        const cards = projects.map((project, index) => {
+            return (
+                <>
+                    <Center
+                        key={index}
+                        height="70vh"
+                        flexDirection="column"
+                        margin="2rem"
+                        className={styles.card}
+                    >
+                        <Image
+                            src={project.imageSrc}
+                            height="600px"
+                            width="300px"
+                            layout="fixed"
+                            alt="project image"
+                        />
+                        <Link style={{marginTop: '1rem'}}>
+                            <Heading size="md">
+                                {project.projectName}
+                            </Heading>
+                        </Link>
+                    </Center>
+
+                </>
+            )
+        })
+
+        return <>{cards}</>
+    }
+
+    return (
+        <>
+            <Layout>
+                <Heading size="xl" textAlign="center" mb="1rem">
+                    Created Things
+                </Heading>
+                <SimpleGrid columns={[1, null, 2]}>
+                    <ProjectCards/>
+                </SimpleGrid>
+            </Layout>
+        </>
+    )
 }
 
 export default Projects
