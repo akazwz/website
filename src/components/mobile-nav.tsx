5b79252c14501c52db85a04c4816cd4b4732d89d
akazwz <akazwz@pm.me>
2022-01-13 17:34:53 +0800

    mobile tab keyboard

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,60 +1,58 @@
-import { FC } from 'react'
 import {
   HStack,
   IconButton,
-  Menu, MenuButton,
+  Menu,
+  MenuButton,
   MenuItem,
   MenuList,
-  Text
+  Link,
+  useDisclosure,
 } from '@chakra-ui/react'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import { useRouter } from 'next/router'
 import ColorModeToggle from './color-mode-toggle'
 import { HeaderTrans } from './header'
 
-const MobileNav: (props: { trans: HeaderTrans }) => JSX.Element = (props:{trans: HeaderTrans}) => {
-  const {trans} = props
+const MobileNav: (props: { trans: HeaderTrans }) => JSX.Element = (props: { trans: HeaderTrans }) => {
+  const { trans } = props
   const router = useRouter()
   return (
     <HStack display={{ base: 'flex', md: 'none' }}>
       <ColorModeToggle/>
       <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label={'toggle show navigation links'}
-          icon={<FaBars/>}
-          variant="outline"
-        />
-        <MenuList>
-          <MenuItem
-            aria-label={'navigate to projects page'}
-            onClick={() => router.push('/projects')}
-          >
-            <Text
-              style={{
-                fontSize: '1.2rem',
-                fontWeight: 'bolder',
-                color: router.route === '/projects' ? '#0070f3' : '',
-              }}
-            >
-              {trans.projects}
-            </Text>
-          </MenuItem>
-          <MenuItem
-            aria-label={'navigate to about page'}
-            onClick={() => router.push('/about')}
-          >
-            <Text
-              style={{
-                fontSize: '1.2rem',
-                fontWeight: 'bolder',
-                color: router.route === '/about' ? '#0070f3' : '',
-              }}
-            >
-              {trans.about}
-            </Text>
-          </MenuItem>
-        </MenuList>
+        {
+          ({ isOpen }) => (
+            <>
+              <MenuButton
+                as={IconButton}
+                aria-label={'toggle show navigation links'}
+                icon={isOpen ? <FaTimes/> : <FaBars/>}
+                variant="outline"
+              />
+              <MenuList>
+                <MenuItem
+                  aria-label={'navigate to projects page'}
+                  _focus={{ textDecoration: 'none', color: '#0070f3' }}
+                  fontWeight={router.route === '/projects' ? 'bolder' : 'normal'}
+                  fontSize={router.route === '/projects' ? 'large' : 'medium'}
+                  onClick={()=>router.push('/projects')}
+              >
+                    {trans.projects}
+                </MenuItem>
+                <MenuItem
+                  aria-label={'navigate to about page'}
+                  _focus={{ textDecoration: 'none', color: '#0070f3' }}
+                  fontWeight={router.route === '/about' ? 'bolder' : 'normal'}
+                  fontSize={router.route === '/about' ? 'large' : 'medium'}
+                  onClick={()=>router.push('/about')}
+
+                >
+                  {trans.about}
+                </MenuItem>
+              </MenuList>
+            </>
+          )
+        }
       </Menu>
     </HStack>
   )
