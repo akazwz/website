8432d38f79a899f8b06904f59739918f5df5650f
akazwz <akazwz@pm.me>
2022-01-15 18:20:41 +0800

    add voice record

--- src/components/layout/header/voice-recognition.tsx
+++ src/components/layout/header/voice-recognition.tsx
@@ -3,33 +3,41 @@ import {
   Box,
   Center,
   Drawer,
+  Circle,
   DrawerBody,
   DrawerCloseButton,
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
   IconButton,
+  Text,
   useColorModeValue,
   useDisclosure,
 } from '@chakra-ui/react'
-import {Voice} from '@icon-park/react'
-import { Circle } from '@chakra-ui/layout'
+import { Voice } from '@icon-park/react'
+import { useReactMediaRecorder } from 'react-media-recorder'
 
-const VoiceRecognition:FC = () => {
+const VoiceRecognition: FC = () => {
   const fillColor = useColorModeValue('black', 'white')
-  const {isOpen, onOpen, onClose} =  useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [isMicReady, setIsMicReady] = useState(false)
   const fill = isMicReady ? 'white' : 'red'
   const circleBg = isMicReady ? 'red' : 'white'
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+    clearBlobUrl,
+  } = useReactMediaRecorder({ audio: true })
 
-  useEffect(()=>{
-    let t = setTimeout(()=>{
+  useEffect(() => {
+    if (status === 'recording') {
       setIsMicReady(true)
-    }, 3000)
-    return function clean() {
-
+      return
     }
-  })
+    setIsMicReady(false)
+  }, [status])
 
   return (
     <>
@@ -40,27 +48,61 @@ const VoiceRecognition:FC = () => {
         variant="ghost"
         color="current"
         icon={<Voice theme="outline" size="24" fill={fillColor}/>}
-        onClick={onOpen}
+        onClick={() => {
+          onOpen()
+          clearBlobUrl()
+          startRecording()
+        }}
       />
       <Drawer
         isOpen={isOpen}
-        placement='top'
-        onClose={onClose}
+        placement="top"
+        onClose={() => {
+          onClose()
+          stopRecording()
+        }}
       >
         <DrawerOverlay/>
         <DrawerContent>
           <DrawerCloseButton/>
           <DrawerHeader>
-            <Center h='15vh' minHeight='150px'>
-              <Circle bg={'transparent'} size='130px' alignContent='center' alignItems='center'>
-                <Circle bg={circleBg} size='78px' alignContent='center' alignItems='center'>
+            <Center h="15vh" minHeight="150px">
+              <Circle bg={'transparent'} size="130px" alignContent="center" alignItems="center">
+                <Circle
+                  bg={circleBg}
+                  size="78px"
+                  alignContent="center"
+                  alignItems="center"
+                  onClick={() => {
+                    switch (status) {
+                      case 'recording':
+                        stopRecording()
+                        return
+                      case 'stopped':
+                        clearBlobUrl()
+                        startRecording()
+                        return
+                    }
+                  }}
+                >
                   <Voice theme="filled" size="48px" fill={fill}/>
                 </Circle>
               </Circle>
             </Center>
           </DrawerHeader>
           <DrawerBody>
-            <Box height='5vh' minHeight='50px'>
+            <Box height="5vh" minHeight="50px">
+              <Text>
+                {status}
+              </Text>
+              <audio
+                hidden
+                src={mediaBlobUrl === null ? undefined : mediaBlobUrl}
+                controls
+                autoPlay
+                loop
+                muted={status === 'recording'}
+              />
             </Box>
           </DrawerBody>
         </DrawerContent>
