3ced7900262384c6403fa0d896a97fa4525d6a4a
akazwz <akazwz@pm.me>
2022-02-02 10:50:36 +0800

    start to get ali yun token

--- pages/api/token.ts
+++ pages/api/token.ts
@@ -4,3 +4,17 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler (req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: 'Token' })
 }
+
+const getToken = () => {
+  const RPCClient = require('@alicloud/pop-core').RPCClient
+  const client = new RPCClient({
+    accessKeyId: '',
+    accessKeySecret: '',
+    endpoint: '',
+    apiVersion: '',
+  })
+
+  client.request('CreateToken').then((result: { token: string }) => {
+    console.log(result.token)
+  })
+}
